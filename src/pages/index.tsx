9d1af569b21b695a1fbe9d8b398a3e32ad0e4254
AlexandreJareck <alexandrejareck@gmail.com>
2021-12-12 16:14:09 -0300

    configuração eslint + prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,6 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-
-  const foo = 'bla'
-
   return (
     <div className={styles.container}>
       <Head>
