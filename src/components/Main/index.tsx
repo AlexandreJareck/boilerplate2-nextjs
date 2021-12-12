718ad5c04c692dac6f20208119ad578017194a31
AlexandreJareck <alexandrejareck@gmail.com>
2021-12-12 18:27:59 -0300

    configuraçoes storybook e plop

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,19 @@
+import { Description, Illustration, Logo, Title, Wrapper } from './styles'
+
 const Main = () => {
   return (
-    <main>
-      <h1>React Avançado</h1>
-    </main>
+    <Wrapper>
+      <Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <Title>React Avançado</Title>
+      <Description>TypeScript, ReactJS, NextJS e Styled Components</Description>
+      <Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código."
+      />
+    </Wrapper>
   )
 }
 
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,13 +1,11 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import Main from '.'
 
 describe('<Main />', () => {
-  it('should rend the heading', () => {
-    render(<Main />)
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
 
-    expect(
-      screen.getByRole('heading', { name: /react avançado/i })
-    ).toBeInTheDocument()
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
